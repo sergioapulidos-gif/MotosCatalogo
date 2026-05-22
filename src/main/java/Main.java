fc4ba85283ceb46bbc01ab955cbb1cf877446638
Sergio Pulido <sergioapulidos@gmail.com>
2026-05-21 21:43:21 -0500

    Inicialización proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import database.ConexionBD;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ConexionBD.conectar();
+    }
+}
---
+++ src/main/java/database/ConexionBD.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBD {
+
+    private static final String URL =
+            "jdbc:postgresql://ep-empty-meadow-apq57n8i-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require";
+
+    private static final String USER =
+            "neondb_owner";
+
+    private static final String PASSWORD =
+            "npg_6muQGwhUtBS8";
+
+    public static Connection conectar() {
+
+        try {
+
+            Connection conexion =
+                    DriverManager.getConnection(
+                            URL,
+                            USER,
+                            PASSWORD
+                    );
+
+            System.out.println("Conexión exitosa a Neon");
+
+            return conexion;
+
+        } catch (Exception e) {
+
+            System.out.println("Error de conexión");
+
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+}
